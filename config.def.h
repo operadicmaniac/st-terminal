835d7b35c068601eccd88ad82658e30546af0c27
Derek Taylor <derek@distrotube.com>
2020-11-20 13:24:45 -0600

    Edited cursor color to make it more visible.

--- config.def.h
+++ config.def.h
@@ -143,8 +143,8 @@ static const char *colorname[] = {
  */
 unsigned int defaultfg = 257;
 unsigned int defaultbg = 256;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+static unsigned int defaultcs = 15;
+static unsigned int defaultrcs = 15;
 
 /*
  * Default shape of cursor
